726ce6632754719f16d8621c49673e14f61f7106
haryu <ryu.axel@gmail.com>
2023-04-20 16:29:28 +0900

    FIX: site, wordpress settings

--- srcs/requirements/bonus/adminer/tools/script.sh
+++ srcs/requirements/bonus/adminer/tools/script.sh
@@ -1,20 +1,42 @@
+# # !/bin/sh
+
+# # Check whether change on configuration file is needed or not 
+# grep -E "listen = 127.0.0.1" /etc/php7/php-fpm.d/www.conf > /dev/null 2>&1
+
+# # If configuration file needs to be changed
+# if [ $? -eq 0 ]; then
+# 	# Change the Listening Host with 8000 port
+# 	sed -i "s/.*listen = 127.0.0.1.*/listen = 8000/g" /etc/php7/php-fpm.d/www.conf
+# fi
+
+# # Check index.php file is exist or not
+# if [ ! -f "/var/www/wordpress/adminer/index.php" ]; then
+# 	# Create the directory on wordpress to serve adminer 
+# 	mkdir -p /var/www/wordpress/adminer 
+# 	# Download a sole adminer page
+# 	curl -s -L https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php --output /var/www/wordpress/adminer/index.php
+# fi
+
+# # Run by Dumb Init
+# php-fpm7 --nodaemonize
+
 # !/bin/sh
 
-# Check whether change on configuration file is needed or not 
+# Check Whether Change on Configuration File is Needed or Not
 grep -E "listen = 127.0.0.1" /etc/php7/php-fpm.d/www.conf > /dev/null 2>&1
 
-# If configuration file needs to be changed
+# If Configuration File Needs to be Changed
 if [ $? -eq 0 ]; then
-	# Change the Listening Host with 8000 port
-	sed -i "s/.*listen = 127.0.0.1.*/listen = 8000/g" /etc/php7/php-fpm.d/www.conf
+  # Change the Listening Host with 8000 Port
+  sed -i "s/.*listen = 127.0.0.1.*/listen = 8000/g" /etc/php7/php-fpm.d/www.conf
 fi
 
-# Check index.php file is exist or not
+# Check Whether index.php File Exists or Not
 if [ ! -f "/var/www/wordpress/adminer/index.php" ]; then
-	# Create the directory on wordpress to serve adminer 
-	mkdir -p /var/www/wordpress/adminer 
-	# Download a sole adminer page
-	curl -s -L https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php --output /var/www/wordpress/adminer/index.php
+  # Create Directory on WordPress to Serve Adminer
+  mkdir -p /var/www/wordpress/adminer
+  # Download a Sole Adminer Page
+  curl -s -L https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php --output /var/www/wordpress/adminer/index.php
 fi
 
 # Run by Dumb Init
