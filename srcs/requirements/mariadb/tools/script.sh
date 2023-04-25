632a9c560ad24492f1e1aa54d8eddb66f94d173c
Ryu(Paul) <80471693+Paul2021-R@users.noreply.github.com>
2023-04-25 17:29:02 +0900

    FIX: script

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,27 +1,28 @@
 # !/bin/sh
 
-# Check Whether already set up or not by custom file
+# Check Whether Already Set Up or Not by Custom File
 cat .setup 2> /dev/null
 
-# If not set up 
+# If Not Set Up Yet
 if [ $? -ne 0 ]; then
-	# Excute MariaDB Daemon as a Background Process to set up
-	/usr/bin/mysqld_safe --datadr=/var/lib/mysql &
-	# Change Config to use socket and network
-	sed -i "s/skip-networking/# skip-networking/g" /etc/my.cnf.d/mariadb-server.cnf
-	# Change Config to Allow Every Host
-	sed -i "s/.*bind-address\s*=.*/bind-address=0.0.0.0\nport=3306/g" /etc/my.cnf.d/mariadb-server.cnf
-	# Check Server status 
-	if ! mysqlamin --wait=30 pinkg; then
-		printf "MariaDB Daemone unreachable\n"
-		exit 1
-	fi 
-	# Read Quenry with Deleteing new lines and Embracing quotes and Eval with Env Varaiabe	
-	eval "echo \"$(cat /tmp/query.sql)\"" | mariadb
-	# Terminate MariaDB
-	pkill mariadb
-	# Make mariadb setup finished
-	touch .setup
-fi	
-# Runt by Dumb Init
-/usr/bin/mysqld_safe --datadir=var/lib/mysql
+  # Execute MariaDB Daemon as a Background Process to Set Up
+  /usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+  # Change Config to Use Not Only Socket But Also Network
+  sed -i "s/skip-networking/# skip-networking/g" /etc/my.cnf.d/mariadb-server.cnf
+  # Change Config to Allow Every Host
+  sed -i "s/.*bind-address\s*=.*/bind-address=0.0.0.0\nport=3306/g" /etc/my.cnf.d/mariadb-server.cnf
+  # Check Server Status Whether Configuration File Applied Well or Not
+  if ! mysqladmin --wait=30 ping; then
+    printf "MariaDB Daemon Unreachable\n"
+    exit 1
+  fi
+  # Read Query with Deleting New Lines and Embracing Quotes and Eval with Env Variables
+  eval "echo \"$(cat /tmp/query.sql)\"" | mariadb
+  # Terminate MariaDB Daemon
+  pkill mariadb
+  # Mark as Set Up Finished
+  touch .setup
+fi
+
+# Run by Dumb Init
+/usr/bin/mysqld_safe --datadir=/var/lib/mysql
